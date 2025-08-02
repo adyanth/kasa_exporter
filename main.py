2a7161786191b87c9ed1f6c360a8c896b593b486
Adyanth Hosavalike <me@adyanth.dev>
2025-08-02 16:48:25 -0600

    fmt

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from prometheus_client import Gauge, start_http_server
 from time import sleep
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
 KASA_USERNAME = environ.get("KASA_USERNAME")
@@ -16,22 +17,27 @@ SCRAPE_SLEEP_INTERVAL = float(environ.get("SCRAPE_SLEEP_INTERVAL", "5"))
 PROMETHEUS_PORT = int(environ.get("PROMETHEUS_PORT", "8000"))
 KASA_PLUGS = environ.get("KASA_PLUGS", "").split(",")
 
+
 async def scrape_ip(
-        dev: Device,
-        p_gauge: Gauge,
-        v_gauge: Gauge,
-        i_gauge: Gauge,
-        today_counter: Gauge,
-        this_month_counter: Gauge
-    ):
+    dev: Device,
+    p_gauge: Gauge,
+    v_gauge: Gauge,
+    i_gauge: Gauge,
+    today_counter: Gauge,
+    this_month_counter: Gauge,
+):
     try:
         await dev.update()
         energy = dev.modules[Module.Energy]
         p_gauge.labels(alias=dev.alias, model=dev.model).set(energy.status.power)
         v_gauge.labels(alias=dev.alias, model=dev.model).set(energy.status.voltage)
         i_gauge.labels(alias=dev.alias, model=dev.model).set(energy.status.current)
-        today_counter.labels(alias=dev.alias, model=dev.model).set(energy.consumption_today)
-        this_month_counter.labels(alias=dev.alias, model=dev.model).set(energy.consumption_this_month)
+        today_counter.labels(alias=dev.alias, model=dev.model).set(
+            energy.consumption_today
+        )
+        this_month_counter.labels(alias=dev.alias, model=dev.model).set(
+            energy.consumption_this_month
+        )
         # print(f"{energy.status=}")
         # print(f"{energy.consumption_today=}")
         # print(f"{energy.consumption_this_month=}")
@@ -44,23 +50,41 @@ async def scrape_ip(
     except:
         print(f"Failed to scrape {dev}")
 
+
 def discover_handler_wrapper():
     plugs = []
+
     async def discover_handler(dev: Device):
         await dev.update()
         if dev.device_type == DeviceType.Plug and dev.has_emeter:
             plugs.append(dev)
             print(f"Discovered {dev.model}: {dev.alias}")
+
     return discover_handler, plugs
+
 
 async def main():
     print("Running main")
 
-    p_gauge = Gauge("plug_power_watts", "Current power consumed in Watts", ["alias", "model"])
-    v_gauge = Gauge("plug_voltage_volts", "Current plug voltage in Volts", ["alias", "model"])
-    i_gauge = Gauge("plug_current_amps", "Current current flow in Amps ", ["alias", "model"])
-    today_counter = Gauge("plug_consumption_today_kwh", "Total energy consumption today in KWh", ["alias", "model"])
-    this_month_counter = Gauge("plug_consumption_this_month_kwh", "Total energy consumtion this month in KWh", ["alias", "model"])
+    p_gauge = Gauge(
+        "plug_power_watts", "Current power consumed in Watts", ["alias", "model"]
+    )
+    v_gauge = Gauge(
+        "plug_voltage_volts", "Current plug voltage in Volts", ["alias", "model"]
+    )
+    i_gauge = Gauge(
+        "plug_current_amps", "Current current flow in Amps ", ["alias", "model"]
+    )
+    today_counter = Gauge(
+        "plug_consumption_today_kwh",
+        "Total energy consumption today in KWh",
+        ["alias", "model"],
+    )
+    this_month_counter = Gauge(
+        "plug_consumption_this_month_kwh",
+        "Total energy consumtion this month in KWh",
+        ["alias", "model"],
+    )
 
     handler, KASA_SMART_PLUGS = discover_handler_wrapper()
     if not KASA_PLUGS or not KASA_PLUGS[0]:
@@ -100,8 +124,9 @@ async def main():
                 )
                 for dev in KASA_SMART_PLUGS
             ]
-        )            
+        )
         sleep(SCRAPE_SLEEP_INTERVAL)
+
 
 if __name__ == "__main__":
     start_http_server(PROMETHEUS_PORT)
